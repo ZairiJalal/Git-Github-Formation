94dba169f0ae17d37267c401e695a504ecb31165
Jalal Zairi <jzairi@SMB.SQLI.COM>
2022-03-14 14:46:55 +0100

    cat feature impl

--- Animal.java
+++ Animal.java
@@ -2,7 +2,16 @@ class Animal {
     public static String getDefault(){
         return "Hello";
     }
+
+    public static String getCatValue(){
+        return "Cat";
+    }
+    
     public static void main(String[] args){
-        System.out.println(getDefault());
+        String txt = getDefault();
+        if(args[0].equals("Cat")){
+            txt = getCatValue();
+        }
+        System.out.println(txt);
     }
 }
