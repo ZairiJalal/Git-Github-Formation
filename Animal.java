640b4b5c66dc6d634f2d48eab6a21bb4d0316fb8
Jalal Zairi <jzairi@SMB.SQLI.COM>
2022-03-14 14:29:52 +0100

    Animal class java

---
+++ Animal.java
@@ -0,0 +1,8 @@
+class Animal {
+    public static String getDefault(){
+        return "Hello";
+    }
+    public static void main(String[] args){
+        System.out.println(getDefault());
+    }
+}
