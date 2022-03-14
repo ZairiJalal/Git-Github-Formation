b35064257307fb033cbd103bfc44f05ab8a02cc8
Jalal Zairi <jzairi@SMB.SQLI.COM>
2022-03-14 14:43:14 +0100

    Bird feature impl

--- Animal.java
+++ Animal.java
@@ -2,7 +2,15 @@ class Animal {
     public static String getDefault(){
         return "Hello";
     }
+     public static String getBirdValue(){
+        return "Tweet";
+    }
+       
     public static void main(String[] args){
-        System.out.println(getDefault());
+        String txt = getDefault();
+        if(args[0].equals("Bird")){
+            txt = getBirdValue();
+        }
+        System.out.println(txt);
     }
 }
